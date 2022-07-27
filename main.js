7a2b29c336c23fcf1b95beab98139f92abc140f3
Matheus Altrão <103171495+MatheusAltrao@users.noreply.github.com>
2022-07-27 16:45:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  var list = document.getElementById("list");
+  list.classList.add("show");
+}
+
+function closeMenu() {
+    var list = document.getElementById("list");
+    list.classList.remove("show");
+  }
